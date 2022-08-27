3223f698d758fde74252b2876db707b3ba130799
thiagorbm <thiagorbm@gmail.com>
2022-08-27 18:03:44 -0300

    tranformado em binário instalável

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 ## O setup.py é transformado para outro formato e colocado no binário do python.
 ## nele ficam informações importantes para que o programa possa ser instalando.
 <<<<<<< HEAD
-## o jeito mais tracional é usando o setuptools. Ele é necessário para 
+## o jeito mais tracional é usando o setuptools. Ele é necessário para
 =======
 ## o jeito mais tracional é usando o setuptools. Ele é necessário para
 >>>>>>> f04db00 (tranformado em binário instalável)
@@ -13,7 +13,7 @@
 ## pip install -e ## se dentro do diretorio atual existe um arquivo setup.py
 ## ao instalar com o -e, alteracoes feitas no repositorio sao automaticamente
 <<<<<<< HEAD
-## "instaladas". 
+## "instaladas".
 =======
 ## "instaladas".
 >>>>>>> f04db00 (tranformado em binário instalável)
@@ -27,14 +27,10 @@ setup(
     version= "0.1.0", ## padrão semantic versioning
     description= "Rewars point system",
     author= "thiago",
-<<<<<<< HEAD
-    packages= find_packages() ## pastas com o programa. Esse comando pega todas as pastas com arquivo init dentro. Pode ser feito manualmente também com listas.
-=======
     packages= find_packages(), ## pastas com o programa. Esse comando pega todas as pastas com arquivo init dentro. Pode ser feito manualmente também com listas.
     entry_points= {
         "console_scripts": [ ## especificar tipo de entrypoint
-            "dundie = dundie.__main__:main" ## nome que será chamado no terminal e o pacote do entrypoint, especificando o modulo (__main__) e a funcao (__main__)
+            "dundie = dundie.__main__:main" ## nome que será chamado no terminal e o pacote do entrypoint, especificando o modulo (__main__) e a funcao zz
         ]
     }
->>>>>>> f04db00 (tranformado em binário instalável)
 )
