ea5a62506656f82f2a2e5004c3db16b3e51a88cf
ThiagoRBM <52140942+ThiagoRBM@users.noreply.github.com>
2022-09-12 14:01:36 -0300

    Package (#21)
    * package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 def read(*paths):
     """Lê arquivos text.
-    >>> read("projeto","VERSION")
+    >>> read("dundie","VERSION")
     '0.1.0'
     >>> read(README.md)
     """
@@ -25,9 +25,12 @@ def read_requierements(path):
 
 
 setup(
-    name="dundie",  # pode ser qualquer coisa. É o nome que será usado do pip
+    name="dundie_trbm",  # pode ser qualquer coisa. É o nome que será usado no
+    # pip. Aparentemente tem que se o mesmo que está no decorator de cli.py
+    # Major.Minor.Patch
+    # X.Y.Z
     version="0.1.0",  # padrão semantic versioning
-    description="Rewars point system",
+    description="Rewards point system",
     # long_description= read_requierements("README.md"), # essa linha estava
     # dando problema na hora de rodar make install, Bruno falou para substituir
     # e funcionou
@@ -35,12 +38,15 @@ setup(
     long_description_content_type="text/markdown",
     author="thiago",
     python_requires=">=3.8",
-    packages=find_packages(),  # pastas com o programa. Esse comando pega todas
-    # as pastas com arquivo init dentro. Pode ser feito manualmente também
-    # com listas.
+    packages=find_packages(exclude=["integration"]),  # pastas com o
+    #  __init__. Esse comando pega todas. Usando exclude, dá para excluir
+    # pastas específicas. Pode ser feito manualmente também com listas.
+    # rodar duas vezes make clean para limpar e fazer outra build
+    include_package_data=True,  # incluir na build coisas além de .py
     entry_points={
-        "console_scripts": [  # especificar tipo de entrypoint
-            "dundie = dundie.__main__:main"  # nome que será chamado no
+         "console_scripts":
+        [  # especificar tipo de entrypoint
+            'dundie=dundie.__main__:main'  # nome que será chamado no
             # terminal e o pacote do entrypoint, especificando o modulo
             # (__main__) e a funcao
         ]
