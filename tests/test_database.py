24ba3dfd5d039ffa064206f00c8a0377d013ffdd
thiagorbm <thiagorbm@gmail.com>
2022-09-27 12:16:24 -0300

    dundie com banco de dados SQL (sqlmodel) e testes passando

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,6 +5,7 @@ from dundie.database import get_session
 from dundie.models import InvalidEmailError, Person
 from dundie.utils.db import add_movement, add_person
 
+
 @pytest.mark.unit
 def test_ensure_database_is_test():
     session = get_session()
@@ -30,22 +31,6 @@ def test_commit_to_database():
         == data["email"]
     )
 
-# @pytest.mark.unit
-# def test_database_commit():
-#     """Testa se as informações estão sendo salvas corretamente no DB"""
-#     db = connect()  # conecta no DB
-#     data = {  # faz alterações, como no nome de alguém
-#         "role": "salesman",
-#         "dept": "sales",
-#         "name": "fulano da silva",
-#     }
-#     db["people"]["fulano@dunder.com"] = data
-#     commit(db)  # faz o commit das alterações
-#     db = connect()  # conecta no DB novamente
-#     assert db["people"]["fulano@dunder.com"] == data
-#     # testa se o nome mudou, inicialmente era apenas fulano e depois do
-#     # commit é para ser fulano da silva
-#     # /tmp/pytest-of-thiagorbm/pytest-current/test_database_commit0
 
 @pytest.mark.unit
 def test_add_person_for_the_first_time():
@@ -66,36 +51,12 @@ def test_add_person_for_the_first_time():
     assert len(person.movement) > 0
     assert person.movement[0].value == 500
 
-# @pytest.mark.unit
-# def test_add_person_for_the_first_time():
-#     pk = "maria@doe.com"  # pessoa que nao existe no DB
-#     data = {"role": "salesman", "dept": "sales", "name": "maria"}
-#     db = connect()
-#     _, created = add_person(db, pk, data)
-#     assert created is True
-#     commit(db)  # comita as alteracoes
-
-#     db = connect()  # conecta novamente ao DB atualizado
-#     assert db["people"][pk] == data
-#     assert db["balance"][pk] == 500  # ele não é manager, então tem que ter 500
-#     assert len(db["movement"][pk]) > 0  # testa que existe a tabela de
-#     # movimentacao, que é o valor inicial
-#     assert db["movement"][pk][0]["value"] == 500  # testa que o valor está
-#     # no índice 0
-#     # /tmp/pytest-of-thiagorbm/pytest-current/test_add_person_for_the_first_0
 
 @pytest.mark.unit
 def test_negative_add_person_invalid_email():
     with pytest.raises(InvalidEmailError):
         add_person({}, Person(email=".@bla"))
 
-
-# @pytest.mark.unit
-# def test_negative_add_person_invalid_email():
-#     """Verifica se emails inválidos resultarão em erros ao invés de serem
-#     adicionados ao DB"""
-#     with pytest.raises(ValueError):
-#         add_person({}, ".@bla", {})
 
 @pytest.mark.unit
 def test_add_or_remove_points_for_person():
@@ -122,25 +83,3 @@ def test_add_or_remove_points_for_person():
     assert after == before - 100
     assert after == 400
     assert before == 500
-
-# @pytest.mark.unit
-# def test_add_or_remove_points_for_person():
-#     pk = "maria@dunder.com"
-#     data = {"role": "salesman", "dept": "sales", "name": "maria"}
-#     db = connect()  # conecta no DB
-#     _, created = add_person(db, pk, data)
-
-#     commit(db)  # comita
-
-#     db = connect()  # reconecta
-#     before = db["balance"][pk]
-
-#     add_movement(db, pk, -100, "manager")  # testando retirar pontos
-#     commit(db)
-
-#     db = connect()
-#     after = db["balance"][pk]
-
-#     assert before == 500
-#     assert after == 400
-#     assert after == before - 100
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,11 +1,10 @@
 import pytest
 
-from dundie.core import load, read
+from dundie.core import read
 from dundie.database import get_session
 from dundie.models import Person
 from dundie.utils.db import add_person
 
-from .constants import PEOPLE_FILE
 
 @pytest.mark.unit
 def test_read_with_query():
@@ -41,34 +40,3 @@ def test_read_with_query():
     response = read(email="joe@doe.com")
     assert len(response) == 1
     assert response[0]["name"] == "Joe Doe"
-
-# @pytest.mark.unit
-# def test_read_with_query():
-#     db = connect()  # conecta no DB
-#     # breakpoint()
-#     pk = "alberto@dunder.com"
-#     data = {"role": "salesman", "dept": "management", "name": "alberto"}
-#     _, created = add_person(db, pk, data)
-#     # breakpoint()
-#     assert created is True
-
-#     pk = "jim2@due.com"
-#     data = {"role": "manager", "dept": "accounting", "name": "Jim Due"}
-#     db = connect()  # conecta no DB
-#     _, created = add_person(db, pk, data)
-#     assert created is True
-#     commit(db)
-#     # breakpoint()  ## aqui
-#     response = read()
-#     # breakpoint()
-#     # assert len(response) == 2  # se não passar nada, recebe os dois usuários
-
-#     response = read(dept="accounting")
-#     assert len(response) == 1
-#     assert response[0]["name"] == "Jim Due"
-#     # funcao read gera uma lista, por isso o indice 0
-#     response = read(email="alberto@dunder.com")
-#     assert len(response) == 1
-#     assert response[0]["name"] == "alberto"
-#     # para rodar apenas esse teste e não todos (com o make test):
-#     # pytest -m "unit" -k read
